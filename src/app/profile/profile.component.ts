61d7d718ac8e189f2ab28708a5da9b046359a365
Your Name <ekka.beaula@gmail.com>
2025-06-02 00:07:57 +0200

    add ProfileComponent; update AppComponent to include ProfileComponent in template and imports; create profile.component.html and profile.component.css with initial content

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,15 @@
 import { Component } from "@angular/core";
 @Component({
   selector: "app-profile",
-  //   templateUrl: "./profile.component.html",
-  //   styleUrl: "./profile.component.css",
-  template: `<h2>Profile</h2>`,
+  templateUrl: "./profile.component.html",
+  styleUrl: "./profile.component.css",
+  // template: `<h1>Profile Component</h1>`,
+  // styles: [
+  //   `
+  //     h1 {
+  //       color: orange;
+  //     }
+  //   `,
+  // ],
 })
 export class ProfileComponent {}
