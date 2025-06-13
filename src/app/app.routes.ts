5f43a9de1c644008f867173463eb82e3a47e5eec
Your Name <ekka.beaula@gmail.com>
2025-06-13 19:22:24 +0200

    feat: update app component structure and add practice route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
 import { LoginComponent } from "./login/login.component";
+import { PracticeComponent } from "./practice/practice.component";
 
 export const routes: Routes = [
   { path: "about", component: AboutComponent },
   { path: "contact", component: ContactComponent },
   { path: "login", component: LoginComponent },
+  { path: "practice", component: PracticeComponent },
 ];
