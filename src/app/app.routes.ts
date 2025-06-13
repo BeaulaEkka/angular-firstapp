91cf3d85cb1cb9b7cc7eda344e8febd9d973fc46
Your Name <ekka.beaula@gmail.com>
2025-06-13 19:28:02 +0200

    feat: update app component template and routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
 import { LoginComponent } from "./login/login.component";
 import { PracticeComponent } from "./practice/practice.component";
+import { HomeComponent } from "./home/home.component";
 
 export const routes: Routes = [
+  { path: "home", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "contact", component: ContactComponent },
   { path: "login", component: LoginComponent },
