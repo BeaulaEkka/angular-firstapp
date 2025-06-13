fd969d7422a806e8245b11183af4b23967de4f0e
Your Name <ekka.beaula@gmail.com>
2025-06-13 15:36:02 +0200

    feat: implement header component with navigation links and update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ import {
   NgSwitch,
   NgSwitchDefault,
 } from "@angular/common";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: "app-root",
@@ -33,7 +34,8 @@ import {
     NgSwitchDefault,
     RouterOutlet,
     RouterLink,
-  ],
+    HeaderComponent
+],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
