5e10887b5fc4de6b16d6817b82bfb6ccaa01b4d7
Your Name <ekka.beaula@gmail.com>
2025-06-14 23:04:54 +0200

    feat: update profile component to display user name from route parameters

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-profile',
+  selector: "app-profile",
   imports: [],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  templateUrl: "./profile.component.html",
+  styleUrl: "./profile.component.css",
 })
 export class ProfileComponent {
-
+  userName: string | null = "";
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    this.userName = this.route.snapshot.paramMap.get("name");
+    console.log(this.userName);
+  }
 }
