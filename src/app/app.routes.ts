273d29c7f75e6a7607be794d32e31ae8ea9697a8
Your Name <ekka.beaula@gmail.com>
2025-06-14 22:13:22 +0200

    feat: add profile route and update navigation links in header and home components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { LoginComponent } from "./login/login.component";
 import { PracticeComponent } from "./practice/practice.component";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { ProfileComponent } from "./profile/profile.component";
 
 export const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "contact", component: ContactComponent },
   { path: "login", component: LoginComponent },
   { path: "practice", component: PracticeComponent },
-  {path:"profile",component:PracticeComponent},
+  { path: "profile", component: ProfileComponent },
   { path: "**", component: PageNotFoundComponent },
 ];
--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-page-not-found',
-  imports: [],
-  templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.css'
+  selector: "app-page-not-found",
+  imports: [RouterLink],
+  templateUrl: "./page-not-found.component.html",
+  styleUrl: "./page-not-found.component.css",
 })
-export class PageNotFoundComponent {
-
-}
+export class PageNotFoundComponent {}
