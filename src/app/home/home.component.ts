f2f75853452ec784a4a1847f8e611bb9512cbad2
Your Name <ekka.beaula@gmail.com>
2025-06-15 19:21:00 +0200

    feat: enhance home component to display user list with dynamic routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,12 +9,17 @@ import { Router, RouterLink } from "@angular/router";
 })
 export class HomeComponent {
   constructor(private router: Router) {}
-//2nd method
+  //2nd method
   // goToProfile(name: string) {
   //   this.router.navigate(["/profile"], { queryParams: { name } });
   // }
 
   //3rd method
-  
 
+  users = [
+    { id: 1, name: "beaula bee", age: 25, email: "beaula@gmail.com" },
+    { id: 2, name: "john doe", age: 30, email: "john@gmail.com" },
+    { id: 3, name: "jane doe", age: 28, email: "jane@gmail.com" },
+    { id: 4, name: "Rina Das", age: 31, email: "rina@gmail.com" },
+  ];
 }
