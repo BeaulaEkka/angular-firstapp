65c35593c53d24d441d4021a2e2a95b50c076ef2
Your Name <ekka.beaula@gmail.com>
2025-06-16 00:41:18 +0200

    feat: implement basic reactive form with username and password fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from "@angular/core";
 import { RouterLink, RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ReactiveFormsModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
-export class AppComponent {}
+export class AppComponent {
+  name = new FormControl();
+  password = new FormControl();
+
+  displayValue() {
+    console.log(this.name.value);
+    console.log(this.password.value);
+  }
+}
