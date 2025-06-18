cda735e5411534c43ca68b420d2bef90f6d5a954
Your Name <ekka.beaula@gmail.com>
2025-06-18 15:05:46 +0200

    feat: integrate Tailwind CSS and update component styles
    - Added Tailwind CSS and PostCSS dependencies in package.json.
    - Created a new CSS class 'passData' for styling in app.component.css.
    - Updated app.component.html to include a user component wrapped in the new class.
    - Modified app.component.ts to import UserComponent.
    - Enhanced home.component.html with a border using Tailwind CSS.
    - Updated user.component.html to display the user input.
    - Refactored user.component.ts to accept user input as an @Input property.
    - Imported Tailwind CSS in global styles.css.
    - Added PostCSS configuration for Tailwind CSS.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { Component } from "@angular/core";
-import { RouterLink, RouterOutlet } from "@angular/router";
-import { HeaderComponent } from "./header/header.component";
-import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { Component } from '@angular/core'
+import { RouterLink, RouterOutlet } from '@angular/router'
+import { HeaderComponent } from './header/header.component'
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
+import { UserComponent } from './user/user.component'
 
 @Component({
-  selector: "app-root",
-  imports: [RouterOutlet, HeaderComponent, ReactiveFormsModule],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, ReactiveFormsModule, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,21 +1,22 @@
-import { Component } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { Component, Input, input } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
-  selector: "app-user",
+  selector: 'app-user',
   imports: [],
-  templateUrl: "./user.component.html",
-  styleUrl: "./user.component.css",
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  name: string | null = "";
-  id: string | null = "";
+  name: string | null = ''
+  id: string | null = ''
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
-      this.id = params["id"];
-      this.name = params["name"];
-    });
+      this.id = params['id']
+      this.name = params['name']
+    })
   }
+  @Input() user: string | null = ''
 }
