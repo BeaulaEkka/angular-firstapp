c1bd033f3b754fef72ed0250f73c2e54a30d6252
Your Name <ekka.beaula@gmail.com>
2025-07-03 23:24:02 +0200

    feat: implement saveUsers method in ApiUsersService for posting user data

--- src/app/apiServices/api-users.service.ts
+++ src/app/apiServices/api-users.service.ts
@@ -13,4 +13,9 @@ export class ApiUsersService {
     const url = 'http://localhost:3000/users'
     return this.http.get<JsonServerUser[]>(url)
   }
+
+  saveUsers(jsonServerUser:JsonServerUser): Observable<JsonServerUser> {
+    const url = 'http://localhost:3000/users'
+    return this.http.post<JsonServerUser>(url )
+  }
 }
